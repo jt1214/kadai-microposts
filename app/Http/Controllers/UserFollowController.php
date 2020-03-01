ca1cd14074be689b573892437ccafe566285fad0
junji.taya <junjitaya1214@gmail.com>
2020-03-01 07:48:01 +0000

    Procfile

--- app/Http/Controllers/UserFollowController.php
+++ app/Http/Controllers/UserFollowController.php
@@ -15,6 +15,6 @@ class UserFollowController extends Controller
     public function destroy($id)
     {
         \Auth::user()->unfollow($id);
-        return back;
+        return back();
     }
 }
